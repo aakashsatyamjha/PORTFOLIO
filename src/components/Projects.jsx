0ecd73b582568a4c68b8562cd2b3c4a8d5f54809
aakash satyam jha <aakash23112004@gmail.com>
2026-04-25 16:42:29 +0530

    Revise CreditWiseLoanSystem project information
    Updated project details for CreditWiseLoanSystem with a new description and tags.

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -20,14 +20,14 @@ const projects = [
     demoLink: "https://profile-card-seven-pi.vercel.app/",
   },
   {
-    title: "CreditW iseLoanSystem",
-    description: "More projects will be added soon",
-    tags: [],
-    color1: "#1a0c2e",
-    color2: "#0f172a",
-    codeLink: "https://github.com/aakashsatyamjha/CreditWiseLoanSystem",
-    demoLink: "https://creditwiseloansystem-fsnwgs6oy8lq2gtp7mfcxs.streamlit.app/",
-  },
+  title: "CreditWiseLoanSystem",
+  description: "ML-based loan approval prediction system",
+  tags: ["ML Models", "Streamlit"],
+  color1: "#1a0c2e",
+  color2: "#0f172a",
+  codeLink: "https://github.com/aakashsatyamjha/CreditWiseLoanSystem",
+  demoLink: "https://creditwiseloansystem-fsnwgs6oy8lq2gtp7mfcxs.streamlit.app/",
+}
 ];
 
 const Projects = () => {
