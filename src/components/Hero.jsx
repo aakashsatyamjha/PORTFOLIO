272d0b91593e9986297992f541bd0c4d9890a4d8
aakash satyam jha <aakash23112004@gmail.com>
2026-04-26 13:17:13 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ import { useTheme } from "../App";
 const handleDownloadCV = () => {
   const link = document.createElement("a");
   link.href = "/cv.pdf";
-  link.download = "Aakash_Satyam_Jha_CV.pdf";
+  link.download = "CV.pdf";
   document.body.appendChild(link);
   link.click();
   document.body.removeChild(link);
